2316838648345117138da73631c2d21ea2aa67ca
Pranav Ojha <pranav.ojha@mail.vinove.com>
2025-10-08 18:43:25 +0530

    ngrok header attempt

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: BACKEND_URL,
 });
+
+if (/ngrok(?:-free)?\.(?:app|io)/i.test(BACKEND_URL)) {
+  axiosInstance.defaults.headers.common['ngrok-skip-browser-warning'] = 'true';
+}
 export default axiosInstance;
